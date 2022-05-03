5a3630ffa43204e4c8e2d7c556fc4f3fac765853
Sid Agrawal <sid@sid-agrawal.ca>
2022-05-03 15:28:43 -0700

    Sid: Add more info in debug_print_bootinfo

--- libsel4debug/src/bootinfo.c
+++ libsel4debug/src/bootinfo.c
@@ -12,6 +12,21 @@
 #include <sel4/sel4.h>
 #include <utils/util.h>
 
+
+char *human_readable_size( size_t size){
+
+    static char buf[32];
+    if (size < 1024) {
+        snprintf(buf, sizeof(buf), "%zu bytes", size);
+    } else if (size < 1024 * 1024) {
+        snprintf(buf, sizeof(buf), "%.1f KB", (float)size / 1024);
+    } else if (size < 1024 * 1024 * 1024) {
+        snprintf(buf, sizeof(buf), "%.1f MB", (float)size / (1024 * 1024));
+    } else {
+        snprintf(buf, sizeof(buf), "%.1f GB", (float)size / (1024 * 1024 * 1024));
+    }
+    return buf;
+}
 void debug_print_bootinfo(seL4_BootInfo *info)
 {
 
@@ -27,22 +42,29 @@ void debug_print_bootinfo(seL4_BootInfo *info)
     printf("Initial thread cnode size: %u\n", (int)info->initThreadCNodeSizeBits);
     printf("List of untypeds\n");
     printf("------------------\n");
-    printf("Paddr    | Size   | Device\n");
+    printf("Paddr    | Size |  SizeBits   | Device\n");
 
     int sizes[CONFIG_WORD_SIZE] = {0};
+    uint64_t total_memory;
     for (int i = 0; i < CONFIG_MAX_NUM_BOOTINFO_UNTYPED_CAPS && i < (info->untyped.end - info->untyped.start); i++) {
+        if (info->untypedList[i].isDevice){
+            continue;
+        }
         int index = info->untypedList[i].sizeBits;
         assert(index < ARRAY_SIZE(sizes));
         sizes[index]++;
-        printf("%p | %zu | %d\n", (void *)info->untypedList[i].paddr, (size_t)info->untypedList[i].sizeBits,
+        size_t size = 1ULL << info->untypedList[i].sizeBits;
+        total_memory += size;
+        printf("%p | %zu | %s | %d\n", (void *)info->untypedList[i].paddr,
+               (size_t)info->untypedList[i].sizeBits,
+               human_readable_size(1ULL<<info->untypedList[i].sizeBits),
                (int)info->untypedList[i].isDevice);
     }
 
-    printf("Untyped summary\n");
+    printf("Untyped summary added: %s\n", human_readable_size(total_memory));
     for (int i = 0; i < ARRAY_SIZE(sizes); i++) {
         if (sizes[i] != 0) {
             printf("%d untypeds of size %d\n", sizes[i], i);
         }
     }
 }
-

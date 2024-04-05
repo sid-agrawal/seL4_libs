af675676f9ee9d94ef006a6f0d1975e97dbbe183
Arya Stevinson <arya.stevinson@gmail.com>
2024-04-05 09:35:08 -0700

    Disable cap tracking

--- libsel4vka/include/vka/capops.h
+++ libsel4vka/include/vka/capops.h
@@ -53,7 +53,7 @@ static inline int vka_cnode_copy(const cspacepath_t *dest, const cspacepath_t *s
 
         cap_info->isMinted = true;
         cap_info->minted_from = src->capPtr;
-        gpi_add_cap_data(cap_info);
+        //gpi_add_cap_data(cap_info);
         // ZF_LOGE("[MINT] Adding info for cap: %lu %lu", cap_info->slot, cap_info->minted_from);
         free(cap_info);
     }
